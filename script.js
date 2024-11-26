70f71b8d3ab9f1ed7a5964ffa3e0cb8c2dd9a4b4
Dhruv-Jain36 <brdt743@gmail.com>
2024-11-26 10:36:00 +0530

    Add getHumanChoice

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function getComputerChoice() {
     let ch = Math.floor(Math.random()*3);
     switch (ch) {
-        case 0: return "Rock";
-        case 1: return "Paper";
-        case 2: return "Scissors";
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let ch = prompt("Rock, paper or scissors?");
+    if (ch.toLowerCase() == 'rock' || ch.toLowerCase() == 'paper' || ch.toLowerCase() == 'scissors') {
+        return ch.toLowerCase();
+    }
+    else {
+        alert("Wrong Choice!");
     }
 }
