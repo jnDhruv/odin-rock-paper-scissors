ec1a592dfb69a17078a992de14baa5e2e3eeed55
Dhruv-Jain36 <brdt743@gmail.com>
2024-11-26 11:04:13 +0530

    Add playRound function
    It determines whether its the user who won or the computer, and increments their score accordingly.

--- script.js
+++ script.js
@@ -14,5 +14,29 @@ function getHumanChoice() {
     }
     else {
         alert("Wrong Choice!");
+        return getHumanChoice();
     }
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log(`No one wins! Both chose ${humanChoice}.`);
+    }
+    else if ((humanChoice == "rock" && computerChoice == "paper")
+        || (humanChoice == "paper" && computerChoice == "scissors")
+        || (humanChoice == "scissors" && computerChoice == "rock")
+    ) {
+        console.log(`Computer won! ${computerChoice} beats ${humanChoice}.`);
+        computerScore++;
+    } else {
+        console.log(`You won! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+}
+
+let humanScore = 0, computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
