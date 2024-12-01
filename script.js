aadd22548000733ccf882127d2c19dffee5c0cb8
Dhruv <dhruvjain.dev@gmail.com>
2024-12-01 23:12:28 +0530

    Add score functionality and optimise readability
    - Changed winning score to 5.
    - Fixed incrementing wrong score variables.
    - Added updateScores().
    - Added a winner announcement message.
    - Added script that stops incrementing the score once a winner has been declared.
    - Added line breaks to improve readability.

--- script.js
+++ script.js
@@ -12,10 +12,12 @@ function getHumanChoice(event) {
 }
 
 function playRound(humanChoice, computerChoice) {
+
     if (humanChoice == computerChoice) {
         result.textContent = `No one wins! Both chose ${humanChoice}.`;
         result.style.color = 'white';
     }
+
     else if ((humanChoice == "rock" && computerChoice == "paper")
         || (humanChoice == "paper" && computerChoice == "scissors")
         || (humanChoice == "scissors" && computerChoice == "rock")
@@ -23,19 +25,50 @@ function playRound(humanChoice, computerChoice) {
         result.textContent = `Computer won! ${computerChoice} beats ${humanChoice}.`;
         result.style.color = 'var(--lose-red-color)';
         computerScore++;
-    } else {
+    } 
+    
+    else {
         result.textContent = `You won! ${humanChoice} beats ${computerChoice}.`;
         result.style.color = 'var(--win-green-color)';
         humanScore++;
     }
+    updateScores();
+}
+
+function updateScores() {
+    humanScoreRef.textContent = humanScore;
+    computerScoreRef.textContent = computerScore;
 }
 
 function driver(choice) {
+    let someoneAlreadyWon = humanScore === 5 || computerScore === 5;
+    if (someoneAlreadyWon) {
+        return;
+    }
+
     playRound(choice,getComputerChoice());
+
+    let someoneWon = humanScore === 5 || computerScore === 5;
+    if (someoneWon) {
+        let finalResult = document.createElement("h2");
+
+        if (humanScore == 5) {
+            finalResult.textContent = `Congrats! You won!`;
+            finalResult.style.color = 'var(--win-green-color)';
+
+        } else {
+            finalResult.textContent = `Oh no! You lost!`;
+            finalResult.style.color = 'var(--lose-red-color)';
+        }
+        document.querySelector(".scoreboard").appendChild(finalResult);
+    }
 }
 
-let humanScore = document.querySelector(".human-score");
-let computerScore = document.querySelector(".computer-score");
+let humanScore = 0;
+let computerScore = 0;
+
+let humanScoreRef = document.querySelector(".human-score h1");
+let computerScoreRef = document.querySelector(".computer-score h1");
 
 let result = document.querySelector(".result");
 
